7fe5065abc551f96fd3007244fe7158c407c6ab7
MarrenWalala <leelash25@gmail.com>
2022-06-05 07:17:31 -0700

    If else statements

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- * main - Prints a random number and states whether it is positive, negative or zero (Entry Point)
+/**
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
