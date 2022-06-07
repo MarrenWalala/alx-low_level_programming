31b7e304960dd2a1fcb6e5454a91935033d01aec
MarrenWalala <leelash25@gmail.com>
2022-06-07 09:35:35 -0700

    Adding two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * add - Entry point
+ * @a: An integer a
+ * @b: An integer b
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	_putchar(int a + int b);
+}
