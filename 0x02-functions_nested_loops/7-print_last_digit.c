d21f648ab41f078caf6a5a70b09f21adcd98b1c9
MarrenWalala <leelash25@gmail.com>
2022-06-08 08:09:49 -0700

    Last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * @a: An integer a
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int a)
+{
+	return (a % 10);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,3 +12,4 @@ int _isalpha(int c);
 int _islower(int c);
 void print_alphabet_x10(void);
 int _abs(int);
+int print_last_digit(int);
