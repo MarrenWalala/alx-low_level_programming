a68732dc30b70ca1587b9a99a78714ff42fcc961
MarrenWalala <leelash25@gmail.com>
2022-06-08 09:12:23 -0700

    Prints Natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_to_98 - Entry point
+ *  @n: A input integer
+ *  Return: Nothing
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,3 +14,4 @@ void print_alphabet_x10(void);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
+void print_to_98(int n);
