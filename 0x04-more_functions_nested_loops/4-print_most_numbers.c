8684f81adbabd19a7d4cc8f3906d21e4000fd4b1
MarrenWalala <leelash25@gmail.com>
2022-06-09 08:48:22 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+	{
+		if (i == '2' && i == '4')
+		{
+			continue;
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
