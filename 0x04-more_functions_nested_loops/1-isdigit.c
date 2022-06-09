89d9697a4a158cc557efcfbeb4ad671e9e0fdd79
MarrenWalala <leelash25@gmail.com>
2022-06-09 08:05:41 -0700

    Checking digits through 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isdigit - Entry point
+ * @c: An input integer
+ * Return: Always 0 (Success)
+ */
+int _isdigit(int c)
+{
+	int digits = 0;
+	int isdigit = 0;
+
+	for (digits = 0; digits <= 10; digits++)
+	{
+		if (c == digits)
+		{
+			isdigit = 1;
+			break;
+		}
+	}
+
+	return (isdigit)
+}
