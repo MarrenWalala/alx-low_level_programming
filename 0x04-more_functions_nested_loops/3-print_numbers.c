191f3d53e24ccbbd44fa3c912522275feca5b85e
MarrenWalala <leelash25@gmail.com>
2022-06-09 08:38:26 -0700

    Printing numbers from 0 through 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
