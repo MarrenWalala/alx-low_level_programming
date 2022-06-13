ace7e79fa480f53c4ef4871b0956951c11fb423d
MarrenWalala <leelash25@gmail.com>
2022-06-13 09:08:35 -0700

    Printing string to the screen

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - Entry point
+ * @str: Character input
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*str[i] != '\0')
+	{
+		_putchar(*str[i]);
+		i++;
+	}
+}
