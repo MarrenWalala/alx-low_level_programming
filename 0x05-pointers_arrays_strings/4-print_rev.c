acf319e7aca11c2804907547798c25192f51a278
MarrenWalala <leelash25@gmail.com>
2022-06-13 09:38:17 -0700

    Reverse string input

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - Entry point
+ * @s: An input string
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	char str_rev = strrev(s);
+	int len = strlen(strrev);
+
+	while (str_rev[len] != '\0')
+	{
+		_putchar(str_rev[len]);
+		len--;
+	}
+	_putchar('\n');
+}
