2665369ce95d2c8508b69646dcc704123b5b64d6
MarrenWalala <leelash25@gmail.com>
2022-06-16 09:12:58 -0700

    concate two str

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ char *_strcat(char *dest, char *src);
 char *leet(char *s);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+char *_strncat(char *dest, char *src, int n);
