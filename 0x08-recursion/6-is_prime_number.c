236824c214f0b0d75fb087e8cbfea3717ec326f6
MarrenWalala <leelash25@gmail.com>
2022-06-22 09:17:14 -0700

    checking for prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,10 +6,29 @@
  */
 int is_prime_number(int n)
 {
-	for (int i = 0; i <= n / 2; i++)
-	{
-		if (n % i == 0)
-			return (0);
+	int div = 2;
+
+	if (n <= 1)
+		return (0);
+
+	if (n <= 3)
 		return (1);
-	}
+
+	return (is_divisible(n, div));
+}
+/**
+ * is_divisible - check if num is divisible
+ * @num: the number to be checked
+ * @div: the result of division
+ * Return: 1 if num is divisible or 0 if numis not divisible
+ */
+int is_divisible(int num, int div)
+{
+	if (num % div == 0)
+		return (0);
+
+	if (div == num / 2)
+		return (1);
+
+	return (is_divisible(num, div + 1));
 }
