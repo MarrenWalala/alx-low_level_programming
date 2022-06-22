2cbcc8f12432f67cdec04e01bac1c3014bad6988
MarrenWalala <leelash25@gmail.com>
2022-06-22 09:01:44 -0700

    Factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - entry point
+ * @n: An input integer
+ * Return: Always 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
