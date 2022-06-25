35e61989aae9bb4a1e6405c9ebec20a115aa8618
MarrenWalala <leelash25@gmail.com>
2022-06-25 13:53:26 +0000

    Printing a program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - Entry point, prints the name of the program
+ * main - Entry point
  * @argc: An input integer
- * @argv: A pointer to a pointer of an array of strings
- * Return: Always 0 (Success)
+ * @argv: A pointer to a string of array
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-	printf("Argc value is %d\n", argc);
-	printf("Program name is %s\n", argv[0]);
+	if (argc > 0)
+		printf("%s\n", argv[0]);
 
 	return (0);
 }
