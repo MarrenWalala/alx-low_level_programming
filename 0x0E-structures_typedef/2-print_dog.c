56bf279500ae24e3680527aeb1586e150887bbb5
MarrenWalala <leelash25@gmail.com>
2022-07-04 16:44:21 +0000

    Print an instance of a struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,9 +12,10 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-
-	if ((*d).name == NULL || (*d).owner == NULL)
-		printf("Name: (nil)");
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
 
 	printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
