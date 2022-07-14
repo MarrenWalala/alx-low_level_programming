7c05c3933ff0816689dd835bcbb90a8a5469a060
MarrenWalala <leelash25@gmail.com>
2022-07-14 19:19:46 +0000

    Adds a new node at the end of the list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,9 +1,8 @@
 #include "lists.h"
-
 /**
  *list_len - A function that gets the number of nodes of linked list
- *@h: head of linked list
- *Return: number of nodes as size_t
+ *@h:head of linked list
+ *Return:number of nodes as size_t
  */
 size_t list_len(const list_t *h)
 {
