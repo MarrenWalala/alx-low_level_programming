fe36afe03ff00422ecf6142cda2fa9bc0d2a6c60
MarrenWalala <leelash25@gmail.com>
2022-07-19 15:56:55 +0000

    Returns number of elements

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -7,9 +7,7 @@ int _putchar(char c);
  *struct listint_s - singly linked list
  *@n: integer
  *@next: points to the next node
- *
  *Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
